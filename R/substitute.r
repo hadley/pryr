5d3d1d79bd213f40be755d35371716b5cd6ab468
hadley <h.wickham@gmail.com>
2013-01-07 17:53:35 -0600

    Document substitute2

--- R/substitute.r
+++ R/substitute.r
@@ -1,11 +1,21 @@
-#' A version of substitute that doesn't evaluate it's first argument
-substitute2 <- function(x, env) {
-  call <- substitute(substitute(x, env), list(x = x))
-  eval(call)
-}
-
+#' A version of substitute that doesn't evaluate it's first argument.
+#'
+#' This version of substitute is needed because \code{substitute} does not
+#' evaluate it's first argument, and it's often useful to be able to modify
+#' a quoted call.
+#'
+#' @param x a quoted call
+#' @param env an environment, or something that behaves like an environment
+#'   (like a list or data frame), or a reference to an environment (like a
+#'   positive integer or name, see \code{\link{as.environment}} for more
+#'   details)
+#' @export
+#' @examples
+#' x <- quote(a + b)
+#' substitute(x, list(a = 1, b = 2))
+#' substitute2(x, list(a = 1, b = 2))
 substitute2 <- function(x, env) {
   stopifnot(is.language(x))
-  env <- as.environment(env)
-  .Call("substitute2", x, env)
+  call <- substitute(substitute(x, env), list(x = x))
+  eval(call)
 }
