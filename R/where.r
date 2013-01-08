aca0ad1afba1637133a8df0191d29379ebd1a7cb
hadley <h.wickham@gmail.com>
2013-01-08 07:37:16 -0600

    Add where funtion

---
+++ R/where.r
@@ -0,0 +1,28 @@
+#' Find where a name is defined.
+#'
+#' Implements the regular scoping rules, but instead of returning the value
+#' associated with a name, it returns the environment in which it is located.
+#'
+#' @param name name, as string, to look for
+#' @param env environment to start at. Defaults to the calling environment
+#'   of this function.
+#' @export
+#' @examples
+#' x <- 1
+#' where("x")
+#' where("t.test")
+#' where("mean")
+#' where("where")
+where <- function(name, env = parent.frame()) {
+  stopifnot(is.character(name), length(name) == 1)
+
+  if (identical(env, emptyenv())) {
+    stop("Can't find ", name, call. = FALSE)
+  }
+
+  if (name %in% ls(env, all.names = TRUE)) {
+    env
+  } else {
+    where(name, parent.env(env))
+  }
+}
