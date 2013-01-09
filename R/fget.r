35782ac2756fd11a5997b26d0b27a14f3e8b57db
hadley <h.wickham@gmail.com>
2013-01-09 08:45:41 -0600

    Add fget function. Not exported

---
+++ R/fget.r
@@ -0,0 +1,18 @@
+#' Find a function with specified name.
+#'
+#' @param name length one character vector giving name
+#' @param env environment to start search in.
+#' @examples
+#' c <- 10
+#' fget("c")
+fget <- function(name, env = parent.frame()) {
+  if (identical(env, emptyenv())) {
+    stop("Could not find function called ", name, call. = FALSE)
+  }
+
+  if (exists(name, env, inherits = FALSE) && is.function(env[[name]])) {
+    env[[name]]
+  } else {
+    fget(name, parent.env(env))
+  }
+}
