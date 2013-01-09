5ba753341665471e4e0022d84ad4df4cf9a43a82
hadley <h.wickham@gmail.com>
2013-01-09 08:05:03 -0600

    Add unenclose function

---
+++ R/unenclose.r
@@ -0,0 +1,24 @@
+#' Unenclose a closure.
+#'
+#' Unenclose a closure by substituting names for values found in the enclosing
+#' environment.
+#'
+#' @param f a closure
+#' @export
+#' @examples
+#' power <- function(exp) {
+#'   function(x) x ^ exp
+#' }
+#' square <- power(2)
+#' cube <- power(3)
+#'
+#' square
+#' cube
+#' unenclose(square)
+#' unenclose(cube)
+unenclose <- function(f) {
+  stopifnot(is.function(f))
+
+  env <- environment(f)
+  make_function(formals(f), substitute2(body(f), env), parent.env(env))
+}
