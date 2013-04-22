9b65c504066e713a4f7b3ca0545433b0d9da1817
hadley <h.wickham@gmail.com>
2013-04-22 07:24:34 -0500

    Capture unevaluated dots

---
+++ R/dots.r
@@ -0,0 +1,29 @@
+#' Capture unevaluated dots.
+#'
+#' @param ...
+#' @return a list of expressions (not expression objects). \code{named_dots}
+#'  will use the deparsed expressions as default names.
+#' @export
+#' @examples
+#' y <- 2
+#' str(dots(x = 1, y, z = ))
+#' str(named_dots(x = 1, y, z =))
+dots <- function(...) {
+  eval(substitute(alist(...)))
+}
+
+#' @rdname dots
+#' @export
+named_dots <- function(...) {
+  args <- dots(...)
+
+  nms <- names(args) %||% rep("", length(args))
+  missing <- nms == ""
+  if (all(!missing)) return(args)
+
+  deparse2 <- function(x) paste(deparse(x, 500L), collapse = "")
+  defaults <- vapply(args[missing], deparse2, character(1), USE.NAMES = FALSE)
+
+  names(args)[missing] <- defaults
+  args
+}
