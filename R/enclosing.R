d119acd1ce7e107ae8c69435da1da5f4cb66651b
hadley <h.wickham@gmail.com>
2014-05-21 09:50:24 -0500

    Add enclosing env function

---
+++ R/enclosing.R
@@ -0,0 +1,14 @@
+#' Find the environment that encloses of a function.
+#'
+#' This is a wrapper around \code{\link{environment}} with a
+#' consistent syntax.
+#'
+#' @param f The name of a function.
+#' @export
+#' @examples
+#' funenv("plot")
+#' funenv("t.test")
+enclosing_env <- function(f) {
+  f <- match.fun(f)
+  environment(f)
+}
